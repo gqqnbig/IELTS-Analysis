b3e924aab10b2db0f25dff128a9108096ffc8a40
QQ喵 <gqqnb2005@gmail.com>
2023-01-03 13:13:12 +0800

    Extract from https://gitlab.com/gqq-my-master/ielts-speaking-tex
    sha 4f0a6f54971c0ea9b819d789c8d90c269d7a4540

---
+++ Program.py
@@ -0,0 +1,210 @@
+import math
+import re
+import sys
+
+import spacy
+from spacy.matcher import Matcher
+
+nlp = spacy.load("en_core_web_sm")
+
+
+def removeSectionHeadings(text, mode: int):
+	"""
+	Remove the Latex section headings
+	:param text:
+	:return: text without headings, and the number of headings removed
+	"""
+	count = 0
+	if mode == 2:
+		text, c = re.subn(r'\\begin{itemize}.+\\end{itemize}', '', text, flags=re.DOTALL)
+		if c == 1:
+			count = 2
+		else:
+			raise Exception('Prompt of Part 2 is expected to be in one itemize environment. But we found ' + str(c))
+
+	text = re.sub(r'\\subsection\*?\{.+?\}', '', text)
+	text, c = re.subn(r'\\subsubsection\*?\{.+?\}', '', text, flags=re.DOTALL)
+	count += c
+	return text, count
+
+
+def checkTenses(doc):
+	matcher = Matcher(nlp.vocab)
+	# https://universaldependencies.org/u/pos/
+	# https://machinelearningknowledge.ai/tutorial-on-spacy-part-of-speech-pos-tagging/
+
+	# ignore Present Simple
+	pattern = [{'TEXT': {'in': ['\'m', 'am', 'is', 'are']}}, {'TAG': 'RB', 'OP': '*'}, {"TAG": "VBG"}]
+	matcher.add("Present Continuous", [pattern])
+
+	pattern = [{'TEXT': {'in': ['have', 'has', '\'ve']}}, {'TAG': 'RB', 'OP': '*'}, {"TAG": "VBN"}]
+	matcher.add("Present Perfect", [pattern])
+
+	pattern = [{'TEXT': {'in': ['have', 'has']}}, {'TEXT': 'been'}, {'TAG': 'RB', 'OP': '*'}, {"TAG": "VBG"}]
+	matcher.add("Present Perfect Continuous", [pattern])
+
+	# ignore Past Simple
+
+	pattern = [{'TEXT': {'in': ['was', 'were']}}, {'TAG': 'RB', 'OP': '*'}, {"TAG": "VBG"}]
+	matcher.add("Past Continuous", [pattern])
+
+	pattern = [{'TEXT': 'had'}, {"TAG": "VBN"}]
+	matcher.add("Past Perfect", [pattern])
+
+	pattern = [{'TEXT': 'had'}, {'TEXT': 'been'}, {'TAG': 'RB', 'OP': '*'}, {"TAG": "VBG"}]
+	matcher.add("Past Perfect Continuous", [pattern])
+
+	pattern = [{'TEXT': 'will'}, {'TEXT': 'be'}, {'TAG': 'RB', 'OP': '*'}, {"TAG": "VBG"}]
+	matcher.add("Future Continuous", [pattern])
+
+	pattern = [{'TEXT': 'will'}, {'TEXT': 'have'}, {"TAG": "VBN"}]
+	matcher.add("Future Perfect", [pattern])
+
+	pattern = [{'TEXT': 'will'}, {'TEXT': 'have'}, {'TEXT': 'been'}, {'TAG': 'RB', 'OP': '*'}, {"TAG": "VBG"}]
+	matcher.add("Future Perfect Continuous", [pattern])
+
+	# for token in doc:
+	# 	print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
+	# 		  token.shape_, token.is_alpha, token.is_stop)
+
+	matches = matcher(doc)
+
+	nonoverlap = []
+	# Matches may have overlaps.
+	for m in matches:
+		if any(m2[1] < m[1] and m2[2] >= m[2] or m2[1] <= m[1] and m2[2] > m[2] for m2 in matches) == False:
+			nonoverlap.append(m)
+
+	tenses = []
+	for m in nonoverlap:
+		# print("{}:\t{}".format(nlp.vocab.strings[m[0]], doc[m[1]:m[2]]))
+		t = nlp.vocab.strings[m[0]]
+		if t not in tenses:
+			tenses.append(t)
+
+	return tenses
+
+
+def checkPassive(doc):
+	matcher = Matcher(nlp.vocab)
+	pattern = [{'LEMMA': 'be'}, {'TAG': 'RB', 'OP': '*'}, {"TAG": "VBN"}]
+	matcher.add("Passive voice", [pattern])
+
+	# for token in doc:
+	# 	print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
+	# 		  token.shape_, token.is_alpha, token.is_stop)
+
+	matches = matcher(doc)
+	# for m in matches:
+	# 	print("{}:\t{}".format(nlp.vocab.strings[m[0]], doc[m[1]:m[2]]))
+	if len(matches) > 0:
+		return ["Passive voice"]
+	else:
+		return []
+
+
+def checkBlackwords(doc):
+	matcher = Matcher(nlp.vocab)
+	pattern = [{'TEXT': 'think'}, {'OP': '!', 'TEXT': 'of'}]
+	matcher.add("think", [pattern])
+
+	matches = matcher(doc)
+	for m in matches:
+		raise Exception(f'Words not allowed: {doc[m[1]:m[2]]}')
+
+
+# print("{}:\t{}".format(nlp.vocab.strings[m[0]], doc[m[1]:m[2]]))
+
+
+def countWords(doc):
+	d = dict()
+	for token in doc:
+		if token.pos_ in ['SPACE', 'PUNCT', 'X', 'SYM']:
+			continue
+
+		if token.lemma_ in d:
+			# Increment count of word by 1
+			d[token.lemma_] = d[token.lemma_] + 1
+		else:
+			# Add the word to dictionary with count 1
+			d[token.lemma_] = 1
+
+	return (dict(sorted(d.items(), key=lambda item: item[1], reverse=True)))
+
+
+if __name__ == '__main__':
+	file = sys.argv[-1]
+	if re.search('part\s?1', file, re.IGNORECASE):
+		mode = 1
+	elif re.search('part\s?2', file, re.IGNORECASE):
+		mode = 2
+	elif len(sys.argv) >= 2:
+		if sys.argv[-2] == '1':
+			mode = 1
+		elif sys.argv[-2] == '2':
+			mode = 2
+		else:
+			raise Exception('File mode is unknown. It must be Part 1 or Part 2. You provided ' + sys.argv[-2])
+	else:
+		raise Exception('File mode is unknown. It must be Part 1 or Part 2.')
+
+	with open(file, encoding='utf8') as f:
+		lines = f.readlines()
+		for i in range(len(lines)):
+			# all lines read by readlines() have trailing \n.
+			lines[i] = lines[i].rstrip()
+			l = lines[i]
+			try:
+				p = l.index('%')
+				l = l[0:p]
+				lines[i] = l
+			except:
+				pass
+
+	for i in range(len(lines)):
+		if i > 0:
+			s = lines[i].lstrip()
+			if s != "" and s[0].islower():
+				lines[i - 1] += " " + lines[i]
+				lines[i] = ""
+
+	text = '\n'.join(lines)
+
+	(text, questionCount) = removeSectionHeadings(text, mode)
+	# remove latex commands, but keep its content
+	text = re.sub(r'\\\w+\{(.+)\}', r'\1', text)
+
+	# text = re.sub(r'[,.;!?] ', ' ', text)
+	# text = re.sub(r'[,.;!?]($|[\n\r])', '', text)
+	# text = re.sub(r'"\s|\s"', '', text)
+
+	doc = nlp(text)
+	checkBlackwords(doc)
+
+	tenses = checkTenses(doc)
+	passives = checkPassive(doc)
+	if len(tenses) + len(passives) <= 2:
+		print('Two few tenses (passive): ' + ', '.join(tenses + passives))
+		print()
+
+	counts = countWords(doc)
+
+	# if counts['I'] > 4 * questionCount:
+	# 	print(f'I: {counts["I"]} - too many')
+
+	counts.pop('the')
+
+	frequentWords = []
+	n = 10
+	for key in list(counts.keys()):
+		if n == 0 or counts[key] <= math.ceil(questionCount * 1.2):
+			break
+		if len(key) <= 2:
+			continue
+
+		frequentWords.append(key + ": " + str(counts[key]))
+		n = n - 1
+
+	if len(frequentWords) > 0:
+		print('Frequent words: ' + ', '.join(frequentWords))
+		print()
---
+++ WordCount.py
@@ -0,0 +1,145 @@
+import glob
+import math
+import os
+import re
+import statistics
+import sys
+
+import spacy
+from spacy.matcher import Matcher
+
+nlp = spacy.load("en_core_web_sm")
+
+import Program
+
+
+def removeSectionHeadings(text):
+	"""
+	Remove the Latex section headings
+	:param text:
+	:return: text without headings, and the number of headings removed
+	"""
+	text = re.sub(r'\\begin{\w+}.+\\end{\w+}', '', text, flags=re.DOTALL)
+
+	text = re.sub(r'\\subsection\*?\{.+?\}', '', text)
+	text = re.sub(r'\\subsubsection\*?\{.+?\}', '', text, flags=re.DOTALL)
+	return text
+
+
+def revealTextInCommands(text):
+	while True:
+		text, n1 = re.subn(r'\\(ps|ss|used|usemore|pr)\{([^\\]+)\}', r'\2', text)
+		text, n2 = re.subn(r'\\topicKeyword{.+?}{(.+?)}', r'\1', text)
+		if n1 + n2 == 0:
+			break
+	return text
+
+
+def removeCommands(text):
+	while True:
+		# \linebreak[1]
+		text, n1 = re.subn(r'\\(linebreak\[\d\]|pause)', r'', text)
+		# \footnote{...}
+		text, n2 = re.subn(r'\\\w+(\{.+?\})*', r'', text)
+		# {\cjkfont ...}
+		if n1 + n2 == 0:
+			break
+	return text
+
+
+def loadTextFromLatexFormat(path):
+	with open(path, encoding='utf8') as f:
+		lines = f.readlines()
+		for i in range(len(lines)):
+			# all lines read by readlines() have trailing \n.
+			lines[i] = lines[i].rstrip()
+			l = lines[i]
+			try:
+				p = l.index('%')
+				l = l[0:p]
+				lines[i] = l
+			except:
+				pass
+
+	for i in range(len(lines)):
+		if i > 0:
+			s = lines[i].lstrip()
+			if s != "" and s[0].islower():
+				lines[i - 1] += " " + lines[i]
+				lines[i] = ""
+
+	text = '\n'.join(lines)
+	# manual hyphenation
+	text = text.replace(r'\-', '')
+
+	try:
+		p = text.index(r'\begin{hints}')
+		text = text[p:]
+	except:
+		pass
+
+	text = removeSectionHeadings(text)
+
+	text = revealTextInCommands(text)
+
+	text = removeCommands(text)
+	return text
+
+
+def getWords(path):
+	text = loadTextFromLatexFormat(path)
+	doc = nlp(text)
+
+	counts = Program.countWords(doc)
+
+	# print(text)
+	c = sum(counts.values())
+
+	print(f'{path}: {c}')
+	return c
+
+
+def get_mean_std(results):
+	# calculate mean
+	mean = statistics.mean(results)
+
+	std = statistics.stdev(results, mean)
+	return mean, std
+
+
+if __name__ == '__main__':
+	# getWords(r'E:\IELTS Speaking\Jerry\Part 2\place for sports.tex')
+	try:
+		p = sys.argv.index('--ref')
+		folder = os.path.abspath(sys.argv[p + 1])
+	except:
+		raise Exception('Reference folder is not given. Use --ref option to provide it.')
+	if not os.path.isdir(folder):
+		raise Exception(f'{folder} is not a folder.')
+
+	targetFiles = []
+	for i in range(len(sys.argv)):
+		f = os.path.abspath(sys.argv[-1 - i])
+		if not os.path.isfile(f) or not f.lower().endswith('.tex'):
+			break
+		targetFiles.append(f)
+
+	if len(targetFiles) == 0:
+		raise Exception(f'No target files (.tex) are provided from command line. {sys.argv}')
+
+	referenceFiles = [f for f in glob.glob(folder + "//*.tex") if f not in targetFiles]
+
+	if len(referenceFiles) < 2:
+		print('No enough files to calculate statistics.')
+		exit(0)
+
+	counts = [getWords(f) for f in referenceFiles]
+
+	mean, std = get_mean_std(counts)
+
+	print(mean, std)
+
+	for t in targetFiles:
+		count = getWords(t)
+		if count < mean - std or count > mean + std:
+			print(f'{os.path.basename(t)}: {count}. The number of words is off range ({mean - std :.2f}, {mean + std :.2f}).', file=sys.stderr)
